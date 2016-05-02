70c59335391e7e0498a46538421b760443df2de4
Bahram Sanaei <sanaei@us.ibm.com>
2016-05-02 15:48:51 -0400

    added the CLOUD_CONTROLLER_API_HOST as an environment property

--- _init.sh
+++ _init.sh
@@ -326,7 +326,7 @@ fi
 # Install cloud-cli for service providers #
 ###########################################
 # setup cloud controller variable 
-if [ -n "$CLOUD_CONTROLLER_API_HOST" ]; then
+if [ -z "$CLOUD_CONTROLLER_API_HOST" ]; then
     export CLOUD_CONTROLLER_API_HOST="https://ace${CF_TARGET}"
 fi
 debugme echo "CLOUD_CONTROLLER_API_HOST:$CLOUD_CONTROLLER_API_HOST"
